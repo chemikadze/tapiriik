9f8d38ea508e027cf0ca5defc8e81963af6dd0c0
Collin Fair <cpf@cpfx.ca>
2013-12-22 21:14:03 -0700

    Endomondo OAuth. Marginally related: upgraded to requests 2.0

--- tapiriik/requests_lib.py
+++ tapiriik/requests_lib.py
@@ -1,5 +1,6 @@
 # For whatever reason there's no built-in way to specify a global timeout for requests operations.
 # socket.setdefaulttimeout doesn't work since requests overriddes the default with its own default.
+# There's probably a better way to do this in requests 2.x, but...
 
 def patch_requests_with_default_timeout(timeout):
 	import requests
@@ -15,5 +16,15 @@ def patch_requests_no_verify_ssl():
 	old_request = requests.Session.request
 	def new_request(*args, **kwargs):
 		kwargs.update({"verify": False})
+		return old_request(*args, **kwargs)
+	requests.Session.request = new_request
+
+def patch_requests_user_agent(user_agent):
+	import requests
+	old_request = requests.Session.request
+	def new_request(*args, **kwargs):
+		headers = kwargs.get("headers",{})
+		headers["User-Agent"] = user_agent
+		kwargs["headers"] = headers
 		return old_request(*args, **kwargs)
 	requests.Session.request = new_request
